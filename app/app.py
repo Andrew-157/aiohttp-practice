b54e1c73915a3e6ba9553923334cb41fc0fcb1a1
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-12 10:03:43 +0200

    using decorator

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 from aiohttp import web
 
 app = web.Application()
+routes = web.RouteTableDef()
 
 
+@routes.get('/')
 async def handler(request):
 
     print("Calling handler...")
     print(request)
     return web.Response(text="Hello AIOHTTP!")
 
-app.add_routes([web.get('/', handler)])
+app.add_routes(routes)
 
 if __name__ == '__main__':
     web.run_app(app)
